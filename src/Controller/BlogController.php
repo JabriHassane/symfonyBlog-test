e332bd9fc33939dd1bfbada2145655aa6cacca06
JabriHassane <75222601+JabriHassane@users.noreply.github.com>
2021-03-12 15:20:02 +0100

    2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Test;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
